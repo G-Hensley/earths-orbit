cb0978c72c925485b9bfd7900f7c51c235273cf9
Gavin Hensley <gavinhensley@protonmail.com>
2025-05-21 04:20:11 -0400

    Add dynamic copyright and website link

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 const btn = document.getElementById("action-btn");
 const universe = document.getElementById("universe");
+const year = document.getElementById("year");
+
+const currentYear = new Date().getFullYear();
+year.textContent = currentYear;
 
 btn.addEventListener("click", () => {
     btn.classList.add("hidden-btn");
